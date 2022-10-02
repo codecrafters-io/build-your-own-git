82068a7e69545a7fb0b621453fe98333de187f4f
nikandfor <nikandfor@gmail.com>
2022-10-02 18:21:33 +0500

    git-starter-go update

---
+++ compiled_starters/git-starter-go/cmd/mygit/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	// Uncomment this block to pass the first stage!
+	//	"os"
+)
+
+// Usage: your_git.sh <command> <arg1> <arg2> ...
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage!
+	//
+	//	switch command := os.Args[1]; command {
+	//	case "init":
+	//		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+	//			if err := os.Mkdir(dir, 0755); err != nil {
+	//				fmt.Printf("Error creating directory: %s\n", err)
+	//			}
+	//		}
+	//
+	//		headFileContents := []byte("ref: refs/heads/master\n")
+	//		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+	//			fmt.Printf("Error writing file: %s\n", err)
+	//		}
+	//
+	//		fmt.Println("Initialized git directory")
+	//
+	//	default:
+	//		fmt.Printf("Unknown command %s\n", command)
+	//		os.Exit(1)
+	//	}
+}
---
+++ compiled_starters/git-starter-go/go.mod
@@ -0,0 +1,3 @@
+module git.codecrafters.io/go-git
+
+go 1.16
