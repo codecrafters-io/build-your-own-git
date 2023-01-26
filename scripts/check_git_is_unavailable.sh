ebeeb5192f75bd481a68ee18c82484298064110e
nikandfor <nikandfor@gmail.com>
2023-01-27 00:26:33 +0400

    general check

--- scripts/check_git_is_unavailable.sh
+++ scripts/check_git_is_unavailable.sh
@@ -11,6 +11,13 @@ tag_name() {
 	echo "git-tester-${base}"
 }
 
+in_image() {
+	local tag="$1"
+	shift
+
+	docker run --rm "$tag" "$@"
+}
+
 ok=0
 
 for f in dockerfiles/*.Dockerfile; do
@@ -18,12 +25,12 @@ for f in dockerfiles/*.Dockerfile; do
 
 	docker build -t "$tag" -f "$f" ./dockerfiles
 
-	if docker run --rm -it "$tag" which git; then
-		# found git
-		ok=1
-	else
+	if in_image "$tag" which sh && ! in_image "$tag" which git; then
 		# ok
 		true
+	else
+		# found git or something failed in general
+		ok=1
 	fi
 
 	docker rmi "$tag"
