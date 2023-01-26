02a9e90779792f0489262928e8d62701eac7d09e
nikandfor <nikandfor@gmail.com>
2023-01-27 00:33:29 +0400

    build image in a starter_template/$lang

--- scripts/check_git_is_unavailable.sh
+++ scripts/check_git_is_unavailable.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+set -eu
+
 tag_name() {
 	dockerfile=$1
 
 	local base
-
-	base=$(basename "$dockerfile")
-	base=${base%.*}
+	base=$(basename "$dockerfile" .Dockerfile)
 
 	echo "git-tester-${base}"
+}
+
+lang_slug() {
+	dockerfile=$1
+
+	local base
+	base=$(basename "$dockerfile" .Dockerfile)
+
+	echo "$base" | cut -d- -f1
 }
 
 in_image() {
@@ -21,9 +30,10 @@ in_image() {
 ok=0
 
 for f in dockerfiles/*.Dockerfile; do
+	lang=$(lang_slug "$f")
 	tag=$(tag_name "$f")
 
-	docker build -t "$tag" -f "$f" ./dockerfiles
+	docker build -t "$tag" -f "$f" "./starter_templates/$lang/"
 
 	if in_image "$tag" which sh && ! in_image "$tag" which git; then
 		# ok
