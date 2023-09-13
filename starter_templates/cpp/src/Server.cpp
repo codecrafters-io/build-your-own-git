d5cb37e175ec65b2975ce549fc2d55e6f3e2bde2
libmartinito <lib.martinito@pm.me>
2023-09-13 18:01:38 +0800

    CC-727 added c++ starter template

---
+++ starter_templates/cpp/src/Server.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    std::cout << "Hello, world!\n";
+
+    // if (argc < 2) {
+    //     std::cerr << "No command provided.\n";
+    //     return EXIT_FAILURE;
+    // }
+
+    // std::string command = argv[1];
+
+    // if (command == "init") {
+    //     try {
+    //         std::filesystem::create_directory(".git");
+    //         std::filesystem::create_directory(".git/objects");
+    //         std::filesystem::create_directory(".git/refs");
+
+    //         std::ofstream headFile(".git/HEAD");
+    //         if (headFile.is_open()) {
+    //             headFile << "ref: refs/heads/master\n";
+    //             headFile.close();
+    //         } else {
+    //             std::cerr << "Failed to create .git/HEAD file.\n";
+    //             return EXIT_FAILURE;
+    //         }
+            
+    //         std::cout << "Initialized git directory\n";
+    //     } catch (const std::filesystem::filesystem_error& e) {
+    //         std::cerr << e.what() << '\n';
+    //         return EXIT_FAILURE;
+    //     }
+    // } else {
+    //     std::cerr << "Unknown command " << command << '\n';
+    //     return EXIT_FAILURE;
+    // }
+
+    // return EXIT_SUCCESS;
+}
