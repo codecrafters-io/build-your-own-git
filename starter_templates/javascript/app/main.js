512d4a46f48bc65cf3ac9aafc78a3e1e5b944c4a
libmartinito <lib.martinito@pm.me>
2023-09-22 15:52:33 +0800

    CC-754 added starter templates

---
+++ starter_templates/javascript/app/main.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+
+// Uncomment this block to pass the first stage
+// const command = process.argv[2];
+//
+// switch(command) {
+//     case "init":
+//         createGitDirectory();
+//         break;
+//     default:
+//         throw new Error(`Unknown command ${command}`);
+// }
+//
+// function createGitDirectory() {
+//     // Creating .git directory and its subdirectories
+//     fs.mkdirSync(path.join(__dirname, ".git"), { recursive: true });
+//     fs.mkdirSync(path.join(__dirname, ".git", "objects"), { recursive: true });
+//     fs.mkdirSync(path.join(__dirname, ".git", "refs"), { recursive: true });
+//   
+//     // Writing to .git/HEAD file
+//     fs.writeFileSync(path.join(__dirname, ".git", "HEAD"), "ref: refs/heads/master\n");
+//     console.log("Initialized git directory");
+// }
