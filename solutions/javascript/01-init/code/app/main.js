09bc807bcf3820a0bef163c6df74f8bc249e45d6
libmartinito <lib.martinito@pm.me>
2023-09-22 16:46:24 +0800

    CC-754 added recompiled files

--- solutions/javascript/01-init/code/app/main.js
+++ solutions/javascript/01-init/code/app/main.js
@@ -3,19 +3,22 @@ const path = require("path");
 
 const command = process.argv[2];
 
-switch(command) {
-    case "init":
-        createGitDirectory();
-        break;
-    default:
-        throw new Error(`Unknown command ${command}`);
+switch (command) {
+  case "init":
+    createGitDirectory();
+    break;
+  default:
+    throw new Error(`Unknown command ${command}`);
 }
 
 function createGitDirectory() {
-    fs.mkdirSync(path.join(__dirname, ".git"), { recursive: true });
-    fs.mkdirSync(path.join(__dirname, ".git", "objects"), { recursive: true });
-    fs.mkdirSync(path.join(__dirname, ".git", "refs"), { recursive: true });
+  fs.mkdirSync(path.join(__dirname, ".git"), { recursive: true });
+  fs.mkdirSync(path.join(__dirname, ".git", "objects"), { recursive: true });
+  fs.mkdirSync(path.join(__dirname, ".git", "refs"), { recursive: true });
 
-    fs.writeFileSync(path.join(__dirname, ".git", "HEAD"), "ref: refs/heads/master\n");
-    console.log("Initialized git directory");
+  fs.writeFileSync(
+    path.join(__dirname, ".git", "HEAD"),
+    "ref: refs/heads/master\n"
+  );
+  console.log("Initialized git directory");
 }
