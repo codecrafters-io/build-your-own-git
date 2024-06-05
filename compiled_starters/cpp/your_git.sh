e7001557807fdb0f9f3d9e3fd8d1955d1b62882f
Ryan <ryan-gg@outlook.com>
2024-06-05 11:11:22 +0000

    fix: update spawn script output path

--- compiled_starters/cpp/your_git.sh
+++ compiled_starters/cpp/your_git.sh
@@ -9,4 +9,4 @@ set -e
 # vcpkg & cmake are required. 
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
 cmake --build ./build
-exec ./server "$@"
+exec ${CODECRAFTERS_SUBMISSION_DIR}/build/server "$@"
