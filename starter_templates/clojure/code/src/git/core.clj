71cdba1777dfe962483ebacbd8942609166e4ff6
Brandon Olivier <brandon@brandonolivier.com>
2025-06-09 16:11:03 -0500

    Add default clojure template code

---
+++ starter_templates/clojure/code/src/git/core.clj
@@ -0,0 +1,11 @@
+(ns git.core
+  (:gen-class))
+
+(defn -main [& args]
+  ;; You can use print statements as follows for debugging, they'll be visible when running tests.
+  (binding [*out* *err*]
+    (println "Logs from your program will appear here!"))
+
+  ;; Uncomment this block to pass the first stage
+  ;;(println "TODO: Implement starter code")
+  )
