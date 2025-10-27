d05a601d6e1141653e232843f70b3039d404e2aa
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-10-27 12:53:46 +0900

    Upgrade Java version to 25 across all relevant files and enable preview features in scripts and configurations. Added a new Dockerfile for Java 25 and updated build properties in pom.xml files.

--- starter_templates/java/code/.codecrafters/run.sh
+++ starter_templates/java/code/.codecrafters/run.sh
@@ -8,4 +8,4 @@
 
 set -e # Exit on failure
 
-exec java -jar /tmp/codecrafters-build-git-java/codecrafters-git.jar "$@"
+exec java --enable-preview -jar /tmp/codecrafters-build-git-java/codecrafters-git.jar "$@"
